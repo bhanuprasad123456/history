7ac132f22bde7cd21289be675daf9fe2fffc3897
bhanuprasad123456 <bhanuprasadccbp@gmail.com>
2023-07-05 17:34:00 +0000

    your message

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import './App.css'
+import BrowserHistory from './BrowserHistory/component'
+// These are the list used in t'he application. You can move them to any component needed.
+
+// Replace your code here
+const App = () => <BrowserHistory />
+
+export default App
---
+++ src/componet/historyitem/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './index.css'
+
+const HistoryItem = props => {
+  const {historyItem, deleteItem} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = historyItem
+
+  const handleClick = () => {
+    deleteItem(id)
+  }
+
+  return (
+    <li>
+      <div className="listItems">
+        <p>{timeAccessed}</p>
+        <img src={logoUrl} alt="domain logo" />
+        <p>{title}</p>
+        <p>{domainUrl}</p>
+      </div>
+
+      <button
+        className="delete-button"
+        onClick={handleClick}
+        data-testid="delete"
+      >
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+          alt="delete"
+          className="delete-icon"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default HistoryItem
